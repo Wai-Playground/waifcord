578ea450b609202a644b00b36503f7895d292d16
Shokkunn <mincl5792@gmail.com>
2023-09-25 15:38:07 -0400

    rename

--- src/utils/path.ts
+++
@@ -1,8 +0,0 @@
-// author = shokkunn
-
-import * as path from 'path';
-
-const projectRoot = path.dirname(Bun.main);
-export function resolveFromRoot(...segments: string[]): string {
-    return path.resolve(projectRoot, ...segments);
-}
