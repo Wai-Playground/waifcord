ef28e1bbe5ec670ca442e128bd82d1c50a44fb67
Shokkunn <mincl5792@gmail.com>
2023-09-27 09:57:21 -0400

    stack iterative approach to validate props

--- src/discord/abstracts/tools/DiscordTool.ts
+++ src/discord/abstracts/tools/DiscordTool.ts
@@ -3,26 +3,6 @@
 import BaseFunctionTool from "../../../agent/abstracts/tools/BaseTool";
 import { AgentFuncInterface } from "../../../agent/abstracts/tools/BaseTool"
 
-interface DiscordPermissionInterface {
-    permissions: {
-        roles: string[];
-        users: string[];
-    }
-}
-
-export interface DiscordToolParameter extends AgentFuncInterface {
-    /**
-     * Soft permission checking:
-     * The AI may use this param to check but since the output is non derterministic, its not a hard check.
-     */    
-    permissions: DiscordPermissionInterface
-}
-
-export interface DiscordToolOptions {
-    permissions: DiscordPermissionInterface
-    requireConfirmation: boolean // whether to require confirmation before executing the tool.
-}
-
 /**
  * @name DiscordTool
  * @description A tool that the AI can use, that can interact with Discord, but is not a command.
@@ -44,4 +24,26 @@ export default class DiscordTool extends BaseFunctionTool {
     override async execute(client: any): Promise<any> {
         
     }
+}
+
+/** Types */
+
+interface DiscordPermissionInterface {
+    permissions: {
+        roles: string[];
+        users: string[];
+    }
+}
+
+export interface DiscordToolParameter extends AgentFuncInterface {
+    /**
+     * Soft permission checking:
+     * The AI may use this param to check but since the output is non derterministic, its not a hard check.
+     */
+    permissions: DiscordPermissionInterface
+}
+
+export interface DiscordToolOptions {
+    permissions: DiscordPermissionInterface
+    requireConfirmation: boolean // whether to require confirmation before executing the tool.
 }
