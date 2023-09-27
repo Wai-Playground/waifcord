d6e3fb7f5102d8b989ec97868b55c957b9e234e2
Shokkunn <mincl5792@gmail.com>
2023-09-27 09:57:39 -0400

    iterative approach to validate props

--- src/agent/abstracts/tools/BaseToolUtils.ts
+++ src/agent/abstracts/tools/BaseToolUtils.ts
@@ -1,42 +1,64 @@
 // author = shokkunn
 
 import _, { keys } from "lodash";
-import { AgentFuncInterface, PropertyTypes, PropertyTypesWithoutDescription } from "./BaseTool";
+import { AgentFuncInterface, PropertyTypes, NestedPropertyTypes } from "./BaseTool";
 import { ChatCompletionCreateParams } from "openai/resources/chat/index.mjs";
 import { encode } from "gpt-3-encoder"
 
 export default class BaseToolUtils {
-    public static validate(input: any, schema: PropertyTypes | PropertyTypesWithoutDescription): boolean {
-        switch (schema.type) {
-            case "boolean":
-                return typeof input === "boolean";
-            case "number":
-                console.log(input)
-                if (typeof input !== "number") return false;
-                if (schema.min !== undefined && input < schema.min) return false;
-                if (schema.max !== undefined && input > schema.max) return false;
-                return true;
-            case "string":
-                return typeof input === "string";
-            case "object":
-                if (typeof input !== "object" || Array.isArray(input)) return false;
-                if (!schema.properties) return true;
-                for (const key of Object.keys(schema.properties)) {
-                    const propSchema = schema.properties[key];
-                    if (propSchema.required && input[key] === undefined) return false;
-                    if (input[key] !== undefined && !this.validate(input[key], propSchema)) return false;
-                }
-                return true;
-            case "array":
-                if (!Array.isArray(input)) return false;
-                if (!schema.itemType) return true;  // if no item type specified, any array is valid
-                for (const item of input) {
-                    if (!this.validate(item, schema.itemType)) return false;
-                }
-                return true;
-            default:
-                return false;
+    public static validateProperty(input: any, schema: PropertyTypes | NestedPropertyTypes): boolean {
+        type StackItem = { input: any; schema: PropertyTypes | NestedPropertyTypes };
+        const stack: StackItem[] = [];
+
+        stack.push({ input, schema });
+
+        while (stack.length > 0) {
+            const { input, schema } = stack.pop()!;
+
+            const withinNumberRange = (value: number, min?: number, max?: number): boolean =>
+                (min === undefined || value >= min) && (max === undefined || value <= max);
+
+            const withinArrayRange = (arr: any[], min?: number, max?: number): boolean =>
+                (min === undefined || arr.length >= min) && (max === undefined || arr.length <= max);
+
+            switch (schema.type) {
+                case "string":
+                case "boolean":
+                    if (typeof input !== schema.type) return false;
+                    break;
+                case "number":
+                    if (typeof input !== "number" || !withinNumberRange(input, schema.min, schema.max)) return false;
+                    break;
+                case "object":
+                    if (typeof input !== "object" || Array.isArray(input)) return false;
+
+                    for (const [key, propSchema] of Object.entries(schema.properties || {})) {
+                        if (propSchema.required && input[key] === undefined) return false;
+                        if (input[key] !== undefined) {
+                            stack.push({ input: input[key], schema: propSchema });
+                        }
+                    }
+                    break;
+                case "array":
+                    if (!Array.isArray(input) || !withinArrayRange(input, schema.min, schema.max)) return false;
+
+                    if (schema.itemType) {
+                        for (const item of input) {
+                            stack.push({ input: item, schema: schema.itemType });
+                        }
+                    }
+                    break;
+            }
         }
+
+        return true;
+    }
+    public static validateFuncResponse(input: any, schema: AgentFuncInterface): boolean {
+        for (const key of Object.keys(schema)) {
+            const propSchema = schema[key];
+            console.log(propSchema)
+        }
+        return true;
     }
 
 
