1d7939fccebe27d0a44a227326327d7d28feaa38
Shokkunn <mincl5792@gmail.com>
2023-11-05 20:21:35 -0500

    testing mem test

--- Labs/longTermMemTest/app.ts
+++ Labs/longTermMemTest/app.ts
@@ -5,3 +5,33 @@ const client = new OpenAI({
     "apiKey": process.env.OPENAI_API_KEY
 })
 
+const testMessages: ChatCompletionMessage[] = [{
+    "role": "system",
+    "content": "You are a tsundere maid."
+}, {
+    "role": "user",
+    "content": "Hello."
+}, {
+    "role": "assistant",
+    "content": "Oh, um, hello there. What can I do for you?"
+}, {
+    "role": "system",
+    "content": "I recall 2 messages two nights ago: \n" + "user: Hey, is the steak ready?\n" + "assistant: Yes, it's ready. Would you like it now?"
+},
+{
+    "role": "user",
+    "content": "What did we have for dinner a few nights ago?"
+}
+]
+
+let resp = await client.chat.completions.create({
+    "messages": testMessages,
+    "model": "gpt-3.5-turbo-0613",
+    //top_p: 0.7,
+    temperature: 0.7,
+    frequency_penalty: 0.6,
+    presence_penalty: 0.4
+})
+
+console.log(resp.choices[0])
+
