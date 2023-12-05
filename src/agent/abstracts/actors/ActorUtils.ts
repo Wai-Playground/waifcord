94afe60d5421b5eca604b693c6988eedcf9ba1db
Shokkunn <mincl5792@gmail.com>
2023-12-05 16:58:36 -0500

    agents

--- src/agent/abstracts/actors/ActorUtils.ts
+++
@@ -1,8 +0,0 @@
-// author = shokkunn
-
-import _ from "lodash";
-
-export default class ActorUtils {
-    static async isProfilePictureValid(url: string) {
-    }
-}
--- src/agent/abstracts/actors/director/Director.ts
+++
@@ -1,5 +0,0 @@
-// author
-
-/**
- * Directors : Creates overarching story lines and plots for the actors and users.
- */
