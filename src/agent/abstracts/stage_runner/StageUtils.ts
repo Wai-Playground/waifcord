5505eddae5bc0ae0fe6cdc23e09df105aff6035a
Shokkunn <mincl5792@gmail.com>
2023-12-12 01:24:33 -0500

    token param

--- src/agent/abstracts/stage_runner/StageUtils.ts
+++ src/agent/abstracts/stage_runner/StageUtils.ts
@@ -7,14 +7,16 @@ export default class StageUtils {
      * @description rewritten Discord API call to edit a webhook since Bun is janky and breaks the original one.
      * @param {string} webhookId to edit
      * @param {object} data {name, channelId, avatar}
+     * @param {string} token the bot token, defaults to Bun.env.BOT_TOKEN
      */
-    async editWebhook(webhookId: string, data: { name: string, channelId: string, avatar: string }) {
+    async editWebhook(webhookId: string, data: { name: string, channelId: string, avatar: string }, token: string | undefined = Bun.env.BOT_TOKEN) {
+        if (!token) throw new Error("No token provided");
         const url = `https://discord.com/api/webhooks/${webhookId}`;
         let res = await fetch(url, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bot ${Bun.env.BOT_TOKEN}`
+                'Authorization': `Bot ${token}`
             },
             body: JSON.stringify(data)
         })
