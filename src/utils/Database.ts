2ab0e2603eab24e9345a410463b5c7ca7ecca5bf
Shokkunn <mincl5792@gmail.com>
2024-01-03 15:21:31 -0500

    restart

--- src/utils/Database.ts
+++ src/utils/Database.ts
@@ -2,34 +2,7 @@
 
 import winston from "winston";
 import { createClient } from 'redis';
-import { Prisma, PrismaClient } from "@prisma/client";
 
-// prisma
-export const prisma = new PrismaClient({
-    "log": [
-        {
-            "emit": "event",
-            "level": "query"
-        },
-        {
-            "emit": "event",
-            "level": "info"
-        },
-        {
-            "emit": "event",
-            "level": "warn"
-        },
-        {
-            "emit": "event",
-            "level": "error"
-        }
-    ]
-});
-
-/** Debug level logging */
-prisma.$on("query", (e: Prisma.QueryEvent) => {
-    winston.debug(`(PRISMA) ${e.query} ${e.duration}ms`);
-});
 
 // redis
 
