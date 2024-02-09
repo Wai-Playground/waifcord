ed00464f3f4e4dd33d9dc7a127583f12b76e0c5a
Shokkunn <mincl5792@gmail.com>
2024-02-09 08:25:01 -0500

    a

--- src/utils/services/Square.ts
+++ src/utils/services/Square.ts
@@ -3,7 +3,7 @@
 import { createClient } from "redis";
 
 if (!Bun.env.REDIS_URI) throw new Error("REDIS_URI not found in environment variables");
-const Square = createClient({url: Bun.env.REDIS_URI});
+const Square = createClient({"url": Bun.env.REDIS_URI});
 await Square.connect();
 
 export default Square;
